a009fd97167fdedbb07ab2b6454301df56507537
William <wkwilliamkunzler@gmail.com>
2023-05-24 10:40:37 -0300

    atualiza 3.0

--- js/login.js
+++ js/login.js
@@ -1,12 +1,35 @@
+function modal(option) {
+    if (!(option == 'open' || option == 'close')) {
+        return;
+    }
+
+    switch (option) {
+        case 'open':
+            document.querySelector('#modal-container').style.display = 'initial';            
+            break;
+        case 'close':
+            document.querySelector('#modal-container').style.display = 'none';            
+            break;
+    }
+}
+
 function enviar(){
     let dados = {
-        "Nome" : document.querySelector("#nome").value,
-        "Matrícula" : document.querySelector("#mat").value,
-        "E-mail" : document.querySelector("#email").value,                  //coloca os valores em um dicionário ordenado
-        "Area" : document.querySelector("#area").value,
-        "Carga Horária" : document.querySelector("#carg").value
-    }
+        "nome" : document.querySelector("#nome").value,
+        "matricula" : document.querySelector("#mat").value,
+        "email" : document.querySelector("#email").value,
+        "area" : document.querySelector("#area").value,
+        "cargahoraria" : document.querySelector("#carg").value
+    };
+
     for (a in dados) {
+        if (dados[a] == "") {
+            modal('open');
+            return;
+        }
+
         localStorage[a] = dados[a];
     }
+
+    window.location.href = '/index.html';
 }
